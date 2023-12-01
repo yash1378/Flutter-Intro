49698f8f54f5c40856af396f633aae0a321ee78c
yash1378 <slayycasm997@gmail.com>
2023-12-01 14:13:23 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MaterialApp(
-//     home: MyHomePage(),
-//   ));
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int myvar = 0;
-//   String col = 'blue';
-//   String txt = 'black';
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: _getColor(),
-//       body: Center(
-//         child: Text(
-//           'You Pressed $myvar times',
-//           style: const TextStyle(
-//             color: _txtColor(),
-//             fontSize: 30.0,
-//             fontWeight: FontWeight.bold,
-//             fontFamily: 'Pacifico',
-//           ),
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () => setState(() {
-//           myvar++;
-//           _changeColor();
-//           _changetxtcolor();
-//         }),
-//         tooltip: 'Increment Counter',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-
-//   Color _getColor() {
-//     switch (col) {
-//       case 'blue':
-//         return Colors.blue;
-//       case 'green':
-//         return Colors.green;
-
-//       // Add more cases for other colors as needed
-//       default:
-//         return Colors.blue;
-//     }
-//   }
-
-//   Color _txtColor() {
-//     switch (txt) {
-//       case 'black':
-//         return Colors.black;
-//       case 'white':
-//         return Colors.white;
-
-//       // Add more cases for other colors as needed
-//       default:
-//         return Colors.black;
-//     }
-//   }
-
-//   void _changeColor() {
-//     // Example: Change color from blue to green and vice versa
-//     setState(() {
-//       col = (col == 'blue') ? 'green' : 'blue';
-//     });
-//   }
-
-//   void _changetxtcolor() {
-//     // Example: Change color from blue to green and vice versa
-//     setState(() {
-//       txt = (txt == 'black') ? 'white' : 'black';
-//     });
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -121,15 +33,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () => setState(() {
-      //     myvar++;
-      //     _changeColor();
-      //     _changeTxtColor(); // Corrected method name
-      //   }),
-      //   tooltip: 'Increment Counter',
-      //   child: const Icon(Icons.add),
-      // ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
